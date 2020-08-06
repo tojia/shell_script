e3f0e0a18865668276f1c5341d3515b0c0b98400
LaTex <32957066+junjiecjj@users.noreply.github.com>
2020-08-06 23:15:45 +0800

    Update systeminfo.sh

--- systeminfo.sh
+++ systeminfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #########################################################################
 # File Name: test.sh
-# Author:陈俊杰
+# Author: 陈俊杰
 # mail: 2716705056@qq.com
 # Created Time: 2019.11.16
 #########################################################################
@@ -9,38 +9,33 @@
 
 :<<EOF
 Linux df 命令用于显示目前在 Linux 系统上的文件系统的磁盘使用情况统计。
-
 语法
-df [选项]... [FILE]...
-文件 - a, --all 包含所有的具有 0 Blocks 的文件系统
-文件 --block-size={SIZE} 使用 {SIZE} 大小的 Blocks
-文件 - h, --human-readable 使用人类可读的格式 (预设值是不加这个选项的...)
-文件 - H, --si 很像 -h, 但是用 1000 为单位而不是用 1024
-文件 - i, --inodes 列出 inode 资讯，不列出已使用 block
-文件 - k, --kilobytes 就像是 --block-size=1024
-文件 - l, --local 限制列出的文件结构
-文件 - m, --megabytes 就像 --block-size=1048576
-文件 --no-sync 取得资讯前不 sync (预设值)
-文件 - P, --portability 使用 POSIX 输出格式
-文件 --sync 在取得资讯前 sync
-文件 - t, --type=TYPE 限制列出文件系统的 TYPE
-文件 - T, --print-type 显示文件系统的形式
-文件 - x, --exclude-type=TYPE 限制列出文件系统不要显示 TYPE
-文件 - v (忽略)
-文件 --help 显示这个帮手并且离开
-文件 --version 输出版本资讯并且离开
-EOF
+ df [选项]... [FILE]...
+ 文件 - a, --all 包含所有的具有 0 Blocks 的文件系统
+ 文件 --block-size={SIZE} 使用 {SIZE} 大小的 Blocks
+ 文件 - h, --human-readable 使用人类可读的格式 (预设值是不加这个选项的...)
+ 文件 - H, --si 很像 -h, 但是用 1000 为单位而不是用 1024
+ 文件 - i, --inodes 列出 inode 资讯，不列出已使用 block
+ 文件 - k, --kilobytes 就像是 --block-size=1024
+ 文件 - l, --local 限制列出的文件结构
+ 文件 - m, --megabytes 就像 --block-size=1048576
+ 文件 --no-sync 取得资讯前不 sync (预设值)
+ 文件 - P, --portability 使用 POSIX 输出格式
+ 文件 --sync 在取得资讯前 sync
+ 文件 - t, --type=TYPE 限制列出文件系统的 TYPE
+ 文件 - T, --print-type 显示文件系统的形式
+ 文件 - x, --exclude-type=TYPE 限制列出文件系统不要显示 TYPE
+ 文件 - v (忽略)
+ 文件 --help 显示这个帮手并且离开
+ 文件 --version 输出版本资讯并且离开
 
-:<<EOF
+
 Linux du 命令用于显示目录或文件的大小。
-
 du 会显示指定的目录或文件所占用的磁盘空间。
-
 语法
-du [-abcDhHklmsSx][-L <符号连接>][-X
-<文件>][--block-size][--exclude=<目录或文件>][--max-depth=<目录层数>][--help][--version][目录或文件]
-参数说明：
-
+ du [-abcDhHklmsSx][-L < 符号连接 >][-X
+<文件>][--block-size][--exclude=< 目录或文件 >][--max-depth=< 目录层数 >][--help][--version][目录或文件]
+ 参数说明：
 -a 或 - all 显示目录中个别文件的大小。
 -b 或 - bytes 显示目录或文件大小时，以 byte 为单位。
 -c 或 --total 除了显示个别目录或文件的大小外，同时也显示所有目录或文件的总和。
@@ -61,99 +56,90 @@ du [-abcDhHklmsSx][-L <符号连接>][-X
 --version 显示版本信息。
 EOF
 
-clear
-echo "
-选择一个选项:
-    1.    系统信息
-    2.    可用磁盘空间和已用磁盘空间
-    3.    /home中每个用户的磁盘使用率
-    4.    查看当前目录下每个文件夹占用的情况:du --max-depth=1 -h
-    5.    查看当前目录的整体占用大小:du -sh
-    6.    查看指定目录下每个文件夹占用的情况:du --max-depth=1 -h /xx/xx/
-    7.    查看指定目录的整体占用大小:du -sh /xx/xx/
-    8.    此电脑的IP地址
-    9.    此电脑(假设使用笔记本电池)的电量剩余
-    10.   此电脑的内存相关信息
-    11.   此电脑的CPU相关信息
-    12.   此电脑的磁盘分区
-    13.   此电脑网卡信息
-    14.   此电脑CPU温度
-    15.   此电脑Vim版本
-    16.   此电脑gcc版本
-    17.   此电脑ipython版本
-    18.   退出
-"
-read -p "请输入您的选项[0~18] > " anum
 
 while true
 do
-    if [[ $anum =~ ^[1-19]$ ]]; then
-        echo "脚本结束."
-        exit
-    fi
+    echo 
+    "
+     选择一个选项:
+        1.    系统信息
+        2.    可用磁盘空间和已用磁盘空间
+        3.    /home 中每个用户的磁盘使用率
+        4.    查看当前目录下每个文件夹占用的情况:du --max-depth=1 -h
+        5.    查看当前目录的整体占用大小:du -sh
+        6.    查看指定目录下每个文件夹占用的情况:du --max-depth=1 -h /xx/xx/
+        7.    查看指定目录的整体占用大小:du -sh /xx/xx/
+        8.    此电脑的 IP 地址
+        9.    此电脑 (假设使用笔记本电池) 的电量剩余
+        10.   此电脑的内存相关信息
+        11.   此电脑的 CPU 相关信息
+        12.   此电脑 CPU 温度
+        13.   此电脑网卡信息
+        14.   此电脑 Vim 版本
+        15.   此电脑 gcc 版本
+        16.   此电脑 ipython 版本
+        0.    退出
+    "
+    read -p " 请输入您的选项 [1~17] > " anum
+    clear
     case ${anum} in
-        1) echo -e "主机姓名: $HOSTNAME\n"
-        uptime
-        # exit
-        ;;
-        2) echo -e "已用磁盘空间和可用磁盘空间:df -ah\n"
-            echo -e "`df -ah`"
-            # exit
+        1) echo -e " 系统信息: $HOSTNAME\n"
+            echo -e "`uptime`"
+            echo -e "`uname -a`"
             ;;
-        3) echo -e "/home/目录下每个用户的总磁盘使用情况:du -sh /home/*\n"
+        2) echo -e " 已用磁盘空间和可用磁盘空间:df -ah\n"
+            echo -e "`df -ah`"
+            ;;
+        3) echo -e "/home/ 目录下每个用户的总磁盘使用情况:du -sh /home/*\n"
             echo -e  "`du -sh /home/*`"
             # exit
             ;;
-        4) echo -e "查看当前目录下每个文件夹的情况:du --max-depth=1 -h\n"
+        4) echo -e " 查看当前目录下每个文件夹的情况:du --max-depth=1 -h\n"
             echo -e "`du --max-depth=1 -h`"
-            # exit
             ;;
-        5) echo "查看当前目录整体占用大小:du -sh\n"
+        5) echo -e " 查看当前目录整体占用大小:du -sh\n"
             echo -e "`du -sh`"
-            # exit
             ;;
-        6) echo "查看指定目录下每个文件夹的情况:du --max-depth -h /xx/xx/xx"
-            echo -e "您想查看哪个目录下的每个文件夹的占用情况？\n"
+        6) echo -e " 查看指定目录下每个文件夹的情况:du --max-depth -h /xx/xx/xx"
+            echo -e " 您想查看哪个目录下的每个文件夹的占用情况？\n"
             read path
             echo -e "`du --max-depth=1 -h ${path}`"
-            # exit
             ;;
-        7) echo "查看指定目录的整体占用大小:du -sh /xx/xx/\n"
-            echo -e "您想查看哪个目录的整体占用情况?\n"
+        7) echo -e " 查看指定目录的整体占用大小:du -sh /xx/xx/\n"
+            echo -e " 您想查看哪个目录的整体占用情况？\n"
             read path
             echo -e "`du -sh ${path}`"
-            # exit
             ;;
-        8) echo "此电脑的IP地址:ip add\n"
-            ip add
-            exit
-            # exit
+        8) echo -e " 此电脑的 IP 地址:ip add\n"
+            echo -e "`ip add`"
             ;;
-        9) echo "此电脑的电池剩余电量:\n"
-            
+        9) echo -e " 此电脑的电池剩余电量:\n"
+            echo -e "`upower -i $(upower -e | grep 'BAT') | grep -E "state|to\ full|percentage"`"
             ;;
+        10) echo -e "此电脑的内存相关信息:free -mh\n"
+            echo -e "`free -mh`"
+            ;;
+        11) echo -e "此电脑的CPU相关信息:lscpu \n"
+            echo -e  "`lscpu`"
+            ;;
+        12) echo -e "此电脑的CPU温度:sensors \n"
+            echo -e "`sensors`"
+            ;;
+        13) echo -e "此电脑的网卡信息:"
+            echo -e "`lspci`"
+            ;;
+        14) echo -e "此电脑的vim版本:vim --version \n"
+            echo -e "`vim --version`"
+            ;;
+        15) echo -e "此电脑gcc版本:\n"
+            echo -e "`gcc -v`"
+            # continue
+            ;;
+        16) echo -e "此电脑python版本\n"
+            echo -e "`python --version`"
+            ;;
+        0) echo "输入错误." >&2
+            exit 1
     esac
-echo "
-选择一个选项:
-    1.    系统信息
-    2.    可用磁盘空间和已用磁盘空间
-    3.    /home中每个用户的磁盘使用率
-    4.    查看当前目录下每个文件夹占用的情况:du --max-depth=1 -h
-    5.    查看当前目录的整体占用大小:du -sh
-    6.    查看指定目录下每个文件夹占用的情况:du --max-depth=1 -h /xx/xx/
-    7.    查看指定目录的整体占用大小:du -sh /xx/xx/
-    8.    此电脑的IP地址
-    9.    此电脑(假设使用笔记本电池)的电量剩余
-    10.   此电脑的内存相关信息
-    11.   此电脑的CPU相关信息
-    12.   此电脑的磁盘分区
-    13.   此电脑网卡信息
-    14.   此电脑CPU温度
-    15.   此电脑Vim版本
-    16.   此电脑gcc版本
-    17.   此电脑ipython版本
-    18.   退出
-"
-read -p "请输入您的选项[1~18] > " anum
-clear
+
 done
