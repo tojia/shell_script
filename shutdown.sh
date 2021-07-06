47b9769d67a20b98d784fe111ba079501f69498d
LaTex <32957066+junjiecjj@users.noreply.github.com>
2021-07-06 22:27:11 +0800

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#关闭伺服界面，检前记录，阵列天线
-ipadd="129.20.1.31 129.20.1.41"
+#关闭 
+ipadd="xxx.xx.x.3x  xxx.xxx.1.41"
 for ip in $ipadd
 do 
 ssh root@$ip shutdown -h
 echo "$ip shutdown"
 done
 
-echo "129.20.1.11 shutdown"
+echo "129.xx.1.xx shutdown"
 sleep 5
 shutdown -h
 
