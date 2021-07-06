0a0df5f139ddf91a26a9ffb1a1b6bae86bcd6f8a
LaTex <32957066+junjiecjj@users.noreply.github.com>
2021-07-06 22:26:30 +0800

    Update remoteShutdown.sh

--- remoteShutdown.sh
+++ remoteShutdown.sh
@@ -2,16 +2,16 @@
 
 ssh-keygen -t rsa
 
-cat /root/.ssh/id_rsa.pub | ssh root@129.20.1.5 'cat >>.ssh/authorized_keys'
+cat /root/.ssh/id_rsa.pub | ssh root@12x.xx.x.x 'cat >>.ssh/authorized_keys'
 
-#关闭伺服界面，检前记录，阵列天线
-ipadd="129.20.1.31 129.20.1.41"
+#关闭伺服 ，检前记 ，阵列 
+ipadd="129.xx.x.xx 129.xx.1.xx"
 for ip in $ipadd
 do 
 ssh root@$ip shutdown -h
 echo "$ip shutdown"
 done
 
-echo "129.20.1.11 shutdown"
+echo "1xx.xx.x.xx shutdown"
 sleep 5
 shutdown -h
