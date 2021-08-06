565ad499c3528d378aa9d32eaa8f13608c949cc3
LaTex <32957066+junjiecjj@users.noreply.github.com>
2021-08-06 18:17:35 +0800

    Add files via upload

--- systeminfo.sh
+++ systeminfo.sh
@@ -126,10 +126,130 @@ linuxmi@linuxmi:~/www.linuxmi.com$ date | lolcat
 但是，现代 Linux 发行版使用 timedatectl 命令查询和更改系统时钟及其设置，并启用或禁用时间同步服务（NTPD 和 co）：
 jack@linux:$ timedatectl | lolcat
 
-
-
-
 EOF
+
+# https://mp.weixin.qq.com/s?__biz=MzU4ODM5ODEwOQ==&mid=2247484098&idx=1&sn=dd597b845bcd64a360c5b90c4277a17e&chksm=fddc29b0caaba0a68344ede3833ef0e127dbf5e2c9d874e18f97c44be9fcc1cfee1f0054a892&mpshare=1&scene=24&srcid=0806adz69kv2fQFlKmHMCC7T&sharer_sharetime=1628226823932&sharer_shareid=0d5c82ce3c8b7c8f30cc9a686416d4a8&exportkey=AfFaEmH0WYWqxGuZcz1zglE%3D&pass_ticket=WXR2HyS%2FM8ykTLfAxxwobbmXUZ1lfTfIfyOHiQBEya1RzNSSn2%2FaXPLSJEqkEnq1&wx_header=0#rd
+# 获取要监控的本地服务器IP地址
+IP=`ifconfig | grep inet | grep -vE 'inet6|127.0.0.1' | awk '{print $2}'`
+echo "IP地址："$IP
+
+# 获取cpu总核数
+cpu_num=`grep -c "model name" /proc/cpuinfo`
+echo "cpu总核数："$cpu_num
+
+# 1、获取CPU利用率
+################################################
+#us 用户空间占用CPU百分比
+#sy 内核空间占用CPU百分比
+#ni 用户进程空间内改变过优先级的进程占用CPU百分比
+#id 空闲CPU百分比
+#wa 等待输入输出的CPU时间百分比
+#hi 硬件中断
+#si 软件中断
+#################################################
+# 获取用户空间占用CPU百分比
+cpu_user=`top -b -n 1 | grep Cpu | awk '{print $2}' | cut -f 1 -d "%"`
+echo "用户空间占用CPU百分比："$cpu_user
+
+# 获取内核空间占用CPU百分比
+cpu_system=`top -b -n 1 | grep Cpu | awk '{print $4}' | cut -f 1 -d "%"`
+echo "内核空间占用CPU百分比："$cpu_system
+
+# 获取空闲CPU百分比
+cpu_idle=`top -b -n 1 | grep Cpu | awk '{print $8}' | cut -f 1 -d "%"`
+echo "空闲CPU百分比："$cpu_idle
+
+# 获取等待输入输出占CPU百分比
+cpu_iowait=`top -b -n 1 | grep Cpu | awk '{print $10}' | cut -f 1 -d "%"`
+echo "等待输入输出占CPU百分比："$cpu_iowait
+
+#2、获取CPU上下文切换和中断次数
+# 获取CPU中断次数
+cpu_interrupt=`vmstat -n 1 1 | sed -n 3p | awk '{print $11}'`
+echo "CPU中断次数："$cpu_interrupt
+
+# 获取CPU上下文切换次数
+cpu_context_switch=`vmstat -n 1 1 | sed -n 3p | awk '{print $12}'`
+echo "CPU上下文切换次数："$cpu_context_switch
+
+#3、获取CPU负载信息
+# 获取CPU15分钟前到现在的负载平均值
+cpu_load_15min=`uptime | awk '{print $11}' | cut -f 1 -d ','`
+echo "CPU 15分钟前到现在的负载平均值："$cpu_load_15min
+
+# 获取CPU5分钟前到现在的负载平均值
+cpu_load_5min=`uptime | awk '{print $10}' | cut -f 1 -d ','`
+echo "CPU 5分钟前到现在的负载平均值："$cpu_load_5min
+
+# 获取CPU1分钟前到现在的负载平均值
+cpu_load_1min=`uptime | awk '{print $9}' | cut -f 1 -d ','`
+echo "CPU 1分钟前到现在的负载平均值："$cpu_load_1min
+
+# 获取任务队列(就绪状态等待的进程数)
+cpu_task_length=`vmstat -n 1 1 | sed -n 3p | awk '{print $1}'`
+echo "CPU任务队列长度："$cpu_task_length
+
+#4、获取内存信息
+# 获取物理内存总量
+mem_total=`free | grep Mem | awk '{print $2}'`
+echo "物理内存总量："$mem_total
+
+# 获取操作系统已使用内存总量
+mem_sys_used=`free | grep Mem | awk '{print $3}'`
+echo "已使用内存总量(操作系统)："$mem_sys_used
+
+# 获取操作系统未使用内存总量
+mem_sys_free=`free | grep Mem | awk '{print $4}'`
+echo "剩余内存总量(操作系统)："$mem_sys_free
+
+# 获取应用程序已使用的内存总量
+mem_user_used=`free | sed -n 3p | awk '{print $3}'`
+echo "已使用内存总量(应用程序)："$mem_user_used
+
+# 获取应用程序未使用内存总量
+mem_user_free=`free | sed -n 3p | awk '{print $4}'`
+echo "剩余内存总量(应用程序)："$mem_user_free
+
+
+# 获取交换分区总大小
+mem_swap_total=`free | grep Swap | awk '{print $2}'`
+echo "交换分区总大小："$mem_swap_total
+
+# 获取已使用交换分区大小
+mem_swap_used=`free | grep Swap | awk '{print $3}'`
+echo "已使用交换分区大小："$mem_swap_used
+
+# 获取剩余交换分区大小
+mem_swap_free=`free | grep Swap | awk '{print $4}'`
+echo "剩余交换分区大小："$mem_swap_free
+
+
+#5、获取磁盘I/O统计信息
+echo "指定设备(/dev/sda)的统计信息"
+# 每秒向设备发起的读请求次数
+disk_sda_rs=`iostat -kx | grep sda| awk '{print $4}'`
+echo "每秒向设备发起的读请求次数："$disk_sda_rs
+
+# 每秒向设备发起的写请求次数
+disk_sda_ws=`iostat -kx | grep sda| awk '{print $5}'`
+echo "每秒向设备发起的写请求次数："$disk_sda_ws
+
+# 向设备发起的I/O请求队列长度平均值
+disk_sda_avgqu_sz=`iostat -kx | grep sda| awk '{print $9}'`
+echo "向设备发起的I/O请求队列长度平均值"$disk_sda_avgqu_sz
+
+# 每次向设备发起的I/O请求平均时间
+disk_sda_await=`iostat -kx | grep sda| awk '{print $10}'`
+echo "每次向设备发起的I/O请求平均时间："$disk_sda_await
+
+# 向设备发起的I/O服务时间均值
+disk_sda_svctm=`iostat -kx | grep sda| awk '{print $11}'`
+echo "向设备发起的I/O服务时间均值："$disk_sda_svctm
+
+# 向设备发起I/O请求的CPU时间百分占比
+disk_sda_util=`iostat -kx | grep sda| awk '{print $12}'`
+echo "向设备发起I/O请求的CPU时间百分占比："$disk_sda_util
+
 
 
 while true
@@ -241,5 +361,5 @@ do
         q) echo "退出......" >&2
             exit 1
     esac
-    
+
 done
